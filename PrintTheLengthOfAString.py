b67f470d048f569efa7ac6c9eb6810098612c360
Reil Tusoy <reiltusoy331@gmail.com>
2024-07-29 15:59:58 +0800

    Adding PrintTheLengthOfAString.py to my github Python_Coding_Problems repo.

---
+++ PrintTheLengthOfAString.py
@@ -0,0 +1,7 @@
+# Print the Length of a String
+# Ask for an input 
+
+print("This program will count the length of characters of any words.")
+word = input("Enter a word: ")
+lenght = len(word)
+print(f"The lenght of the word {word} is: \"{lenght}\"")
