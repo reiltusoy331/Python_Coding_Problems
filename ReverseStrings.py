0eccddc2e62fbb48dc359f047f4126895e5c57cb
Reil Tusoy <reiltusoy331@gmail.com>
2024-07-30 15:46:24 +0800

    Adding ReverseStrings.py to my github Python_Coding_Problems repo.

---
+++ ReverseStrings.py
@@ -0,0 +1,6 @@
+print("This problam will print the entered word in reverse order. \n")
+
+word = input("Enter a word: ")
+print(f"The reverse order of word \"{word}\" is \"{word[::-1]}\".")
+print(f"The entered word \"{word}\" has {len(word)} letters.")
+print()
