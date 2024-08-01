bdead00181abf7aaa4bc3f2684dc88a4782057ea
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-01 13:10:18 +0800

    Adding PatternProgram2.py to my github Python_Coding_Problems repo.

---
+++ PatternProgram2.py
@@ -0,0 +1,19 @@
+# Declare a variable name "num_row" 
+# The said variable will dictate the number of rows of the triangle pattern
+### Target Output ###
+# * * * * 
+#  * * *  
+#   * *
+#    *
+
+
+num_row=4
+
+for i in range(num_row):
+    #space
+    for space in range(i):
+        print(" ",end='')
+    #star
+    for star in range(num_row-i):
+        print("* ",end='')
+    print()
