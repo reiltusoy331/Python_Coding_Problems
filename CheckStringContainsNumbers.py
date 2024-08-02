99db74bdf40bc3937c9311e194f2b535484b4405
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-02 13:08:02 +0800

    Adding CheckStringContainsNumbers.py to my github Python_Coding_Problems repo.

---
+++ CheckStringContainsNumbers.py
@@ -0,0 +1,17 @@
+#---------
+# Option 1
+#---------
+word1 =""
+
+if word1.isdigit() == True:
+    print("True")
+else:
+    print("False")
+
+
+#---------
+# Option 2
+#---------
+word2 ="1234"
+
+print(word2.isdigit())
