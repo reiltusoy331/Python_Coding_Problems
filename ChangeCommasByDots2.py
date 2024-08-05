14d9033558fe576a5d6eedd3a8427b52eca6577c
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-05 15:35:42 +0800

    Adding ChangeCommasByDots2.py to my github Python_Coding_Problems repo.

---
+++ ChangeCommasByDots2.py
@@ -0,0 +1,13 @@
+# Write a Python program that prints a version of the string variable "word" 
+# with all commas replaced by dots.
+
+#-----------
+# Option 2
+#-----------
+
+word = "Hello, World!"
+char= ","
+replace ="."
+
+print(word.replace(char,replace))
+
