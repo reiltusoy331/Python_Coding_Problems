479407b655a5cff8358a666904d8187327a7a9f3
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-13 12:28:48 +0800

    Adding RemoveSpaces2.py to my github Python_Coding_Problems repo.

---
+++ RemoveSpaces2.py
@@ -0,0 +1,10 @@
+# Write a Python program that prints a copy of the string word without any spaces.
+# Words should be connected in the final string.
+# If the string doesn't contain spaces, print it intact.
+
+#---------
+# Option 2
+#---------
+word = "Hello, World"
+
+print(word.replace(" ",""))
