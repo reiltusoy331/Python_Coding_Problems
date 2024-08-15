9a4f7305968a57fc037b806d4b9e3b8a1be9adca
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-15 14:03:25 +0800

    Adding CheckSuffix2.py to my github Python_Coding_Problems repo.

---
+++ CheckSuffix2.py
@@ -0,0 +1,12 @@
+# Write a Python program that checks if the string s ends with a specific sequence of characters 
+# denoted by the variable suffix.
+# If it does, print True. Else, print False.
+# This test should be case sensitive. Therefore, "A" should not be equivalent to "a".
+#---------
+# Option 2
+#---------
+
+word = "Teammate"
+suffix = "mate"
+
+print(word.endswith(suffix))
