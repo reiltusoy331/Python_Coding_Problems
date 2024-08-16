1ea9b89b08561493dc84e7b4bbba2d084dc5b1f8
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-16 14:26:18 +0800

    Adding CountRepeatedLetters2.py to my github Python_Coding_Problems repo.

---
+++ CountRepeatedLetters2.py
@@ -0,0 +1,25 @@
+# Write a Python program to count the number of repeated letters in the string "word".
+# The program must print the total number of repeated letters and a message on the 
+# next line displaying the repeated characters separated by a space and sorted alphabetically.
+# If there are no repeated characters in the string, print 0 as the total count and None on the next line.
+
+#---------
+# Option 2
+#---------
+
+word = "letters"
+letters =[]
+letter_counter = 0
+
+for letter in word:
+    if (word.count(letter) >1 ) and (letter not in letters):
+        letter_counter+=1
+        letters.append(letter)
+    
+print(letter_counter)
+
+if letters:
+    print(*sorted(letters),sep=" ")
+else:
+    print(None)
+
