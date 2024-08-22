f04b9fd6e212295e241815452fa2cc162e555ae9
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-22 15:07:37 +0800

    Adding multipleListElement3.py to my github Python_Coding_Problems repo.

---
+++ multipleListElement3.py
@@ -0,0 +1,17 @@
+# Write a Python program that multiplies all the items in a list by the value of the 
+# variable factor. The program must print the list as the output.
+# The program should also allow multiplying the variable factor by a string in case the 
+# list contains strings. You may assume that the value of factor will be a positive integer.
+
+#---------- 
+# Option 3
+#----------
+
+list = [3,4,5,6]
+factor = 2
+
+
+for i, element in enumerate(list):
+    list[i] =element*factor
+    
+print(list)
