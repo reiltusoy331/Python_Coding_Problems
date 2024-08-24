7cf1121b2a7801b000305b0752589fb86e87d2ee
Reil Tusoy <reiltusoy331@gmail.com>
2024-08-24 10:38:00 +0800

    Adding CheckListIfEmpty1.py to my github Python_Coding_Problems repo.

---
+++ CheckListIfEmpty1.py
@@ -0,0 +1,21 @@
+# Write a Python program that checks if a list is empty or not.
+# If the list is empty, print "Empty". Else, print "Not Empty".
+
+###########
+# Option 1
+###########
+
+List1 =[1,2,3,4]
+List2 =[]
+
+if len(List1) != 0:
+    print("Not Empty")
+else:
+    print("Empty")
+
+print()
+
+if len(List2) != 0:
+    print("Not Empty")
+else:
+    print("Empty")
