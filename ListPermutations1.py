b4f9539aa5f22bf8429c0742fc49a467782de7be
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-04 14:16:00 +0800

    Adding ListPermutations1.py to my github Python_Coding_Problems repo.

---
+++ ListPermutations1.py
@@ -0,0 +1,24 @@
+# Write a Python program that generates and prints all the possible permutations of a list.
+# A permutation is a possible arrangement of the elements of the list. For example, 
+# [2, 1, 3] is a permutation of [1, 2, 3].
+# Print each permutation as a list on a separate line. You can print them as lists or tuples.
+# Include the list itself as a permutation.
+# HINT: The permutations function of the itertools module can be very helpful to solve this exercise. 
+# You can import this module with import itertools.
+
+##########
+# Option 1
+##########
+
+from itertools import permutations
+
+my_list = [1,2,3]
+perm = permutations(my_list,3)
+
+for perm_list in perm:
+    print(perm_list)
+    
+
+
+
+
