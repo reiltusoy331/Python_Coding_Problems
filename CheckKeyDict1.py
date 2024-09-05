2d1e4eb000fd8abe62f40a894fcca36646e7b72c
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-05 15:01:20 +0800

    Adding CheckKeyDict1.py to my github Python_Coding_Problems repo.

---
+++ CheckKeyDict1.py
@@ -0,0 +1,19 @@
+# Write a Python program that checks if a key exists in a dictionary or not.
+# If the key exists in the dictionary, print True. Else, print False.
+# The key should be stored in the variable key.
+
+###########
+# Option 1
+###########
+
+my_dict = {"a":4,"b":6,"c":8}
+key ="d"
+
+if key in my_dict:
+    print(True)
+else:
+    print(False)
+
+
+    
+
