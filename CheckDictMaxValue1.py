1059fd845de7cb4d950357623a20c97942a0a5c6
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-11 11:33:45 +0800

    Adding CheckDictMaxValue1.py to my github Python_Coding_Problems repo.

---
+++ CheckDictMaxValue1.py
@@ -0,0 +1,28 @@
+# Write a Python program that checks if all values in a dictionary are equal.
+# If they are, print True. Else, print False.
+# If the dictionary is empty, print "Empty".
+
+#########
+#Option 1
+#########
+
+my_dict = {"a":4,"b":4}
+
+if len(my_dict.values()) == 0:
+    print(None)        
+else:
+    max_value = max(my_dict.values())
+    print(max_value)
+    
+
+
+
+
+
+
+
+
+
+
+
+
