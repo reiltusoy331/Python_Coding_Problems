dc69542d0acde5ba3c25e234da1ea09b6b03620a
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-26 09:22:45 +0800

    Adding CompFourSeasons2.py to my github Python_Coding_Problems repo.

---
+++ CompFourSeasons2.py
@@ -0,0 +1,22 @@
+# Write a Python program that prints the corresponding season based on the value of the variable season_num.
+# The possible values of season_num are: 1 for Spring, 2 for Summer, 3 for Fall, 4 for Winter.
+# If the value of season_num is neither one of these values, print "Please enter a valid number"
+
+##########
+# Option 2
+##########
+
+season_num = 3
+
+match season_num:
+    case 1:
+        print("Spring")
+    case 2:
+        print("Spring")
+    case 3:
+        print("Spring")
+    case 4:
+        print("Spring")
+    case 5:
+        print("Please enter a valid number.")
+
