7118a34f9c59728c6c9ddee108a14ff77e5f115d
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-28 09:47:03 +0800

    Adding CheckLeapYearOrNot2.py to my github Python_Coding_Problems repo.

---
+++ CheckLeapYearOrNot2.py
@@ -0,0 +1,24 @@
+# Write a Python program that prints if a given year was (or will) be a leap year.
+# -> This is how you can determine if a year is a leap year or not:
+# -> if (year is not divisible by 4) then (it is a common year).
+# -> else if (year is not divisible by 100) then (it is a leap year)
+# -> else if (year is not divisible by 400) then (it is a common year)
+# -> else (it is a leap year)
+##########
+# Option 2
+##########
+
+input_year = 2025
+
+if input_year % 400 == 0:
+    print(f"{input_year} is a leap year.")
+
+elif input_year % 100 == 0:
+    print(f"{input_year} is not a leap year.")
+
+elif input_year % 4 == 0:
+    print(f"{input_year} is a leap year.")
+
+else:
+    print(f"{input_year} is not a leap year.")
+
