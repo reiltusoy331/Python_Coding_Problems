e80247c65cb0e7a83e3e968598aaed430267dd34
Reil Tusoy <reiltusoy331@gmail.com>
2024-09-29 14:26:25 +0800

    Adding MaxThreeNumbers1.py to my github Python_Coding_Problems repo.

---
+++ MaxThreeNumbers1.py
@@ -0,0 +1,18 @@
+# Write a Python program that prints the maximum of three integers (a, b, c).
+
+##########
+# Option 1
+##########
+
+a=3
+b=3
+c=3
+
+if (a > b) and (a > c):
+    print(a)
+
+elif (b > a) and (b > c):
+    print(b)
+
+else:
+    print(c)
