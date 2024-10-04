866cfba0d65ca2822dcd1a18add4c3d1be9a855f
Reil Tusoy <reiltusoy331@gmail.com>
2024-10-04 10:29:06 +0800

    Adding CompareThreeNumbers.py to my github Python_Coding_Problems repo.

---
+++ CompareThreeNumbers.py
@@ -0,0 +1,11 @@
+# Write a Python program that prints "Equal" if three numbers a, b, and c are equal.
+# If at least one number if different, the program should print "Not Equal".
+
+a = 3
+b = 3
+c = 2
+
+if (a == b ) and (b == c):
+    print("Equal")
+else:
+    print("Not Equal")
