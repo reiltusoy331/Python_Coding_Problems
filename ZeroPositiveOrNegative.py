8fb6bd49dbe973f0346efe1f3e1757e248792268
Reil Tusoy <reiltusoy331@gmail.com>
2024-10-08 11:11:25 +0800

    Adding ZeroPositiveOrNegative.py to my github Python_Coding_Problems repo.

---
+++ ZeroPositiveOrNegative.py
@@ -0,0 +1,12 @@
+
+#Write a Python program that prints if a number num is either "Positive", "Negative", or "Zero".
+
+input = -1
+
+if input == 0:
+    print('Zero')
+elif input >0:
+    print('Positive')
+else:
+    print('Negative')
+
