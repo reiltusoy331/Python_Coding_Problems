8c3d45ae2e737d66a2dab52ba41ccf5c2d24e489
Reil Tusoy <reiltusoy331@gmail.com>
2024-10-11 15:58:12 +0800

    Adding PrintIntegersReverseOrder4.py to my github Python_Coding_Problems repo.

---
+++ PrintIntegersReverseOrder4.py
@@ -0,0 +1,11 @@
+# Write a Python program that prints the integers between a given number n and 1 (in descending order, both inclusive).
+# The value of n should be entered by the user and you may assume that it is an integer greater than or equal to 1.
+# Use a loop to print each number on a separate line.
+
+# Using "reversed" function
+print('Horizontally print from given number of "n" to 1.')
+
+user_input=int(input("Enter the value of \"n\": "))
+
+for i in reversed(range(1,user_input+1)):
+    print(i,end=' ')
