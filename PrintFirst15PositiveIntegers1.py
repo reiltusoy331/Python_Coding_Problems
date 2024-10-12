ec634b668836bd9eff2e4d19119220c4ddcfd537
Reil Tusoy <reiltusoy331@gmail.com>
2024-10-12 15:21:19 +0800

    Adding PrintFirst15PositiveIntegers1.py to my github Python_Coding_Problems repo.

---
+++ PrintFirst15PositiveIntegers1.py
@@ -0,0 +1,8 @@
+# Write a Python program that prints the first 15 positive integers (starting from 1).
+# Print the numbers one per line using a loop and the range() function.
+
+print('Vertically print first 15 positive integers.')
+
+for i in range(1,16):
+    print(i)
+
