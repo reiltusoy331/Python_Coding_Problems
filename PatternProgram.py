2e68bfb834865a685be5f5da68b0233c1a19de21
Reil Tusoy <reiltusoy331@gmail.com>
2024-11-10 19:07:27 +0800

    Adding PatternProgram.py to my github Python_Coding_Problems repo.

---
+++ PatternProgram.py
@@ -0,0 +1,16 @@
+### Target Output ###
+#    * * * *
+#    * * * *
+#    * * * *
+#    * * * *
+
+asterisk = 4
+
+for i in range(asterisk):
+    for j in range(asterisk):
+        print("*",end=' ')
+    print()
+
+# shorter version of the code
+# for i in range(asterisk):
+#     print("* "*asterisk)
