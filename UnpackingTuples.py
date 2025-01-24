5354617a17670cd45b63cc885103fc860e0a44af
Reil Tusoy <reiltusoy331@gmail.com>
2025-01-25 03:44:59 +0800

    Adding UnpackingTuples.py to my github Python_Coding_Problems repo.

---
+++ UnpackingTuples.py
@@ -0,0 +1,9 @@
+# packing tuples
+prices = (0.5,1.5,2.5)
+
+# unpacking tuples 
+mango,banana,apple = prices
+
+print(f"Price of mango is ${mango}.")
+print(f"Price of banana is ${banana}.")
+print(f"Price of apple is ${apple}.")
